679f6206731a53b7cb2acde37ec51a193bb00006
Loïc Remy <loic.remy@hotmail.com>
2021-04-23 23:58:08 +0200

    sort function works. Problems with eventListener

--- js/controllers/controllerUsers.js
+++ js/controllers/controllerUsers.js
@@ -27,13 +27,38 @@ class ControllerUsers extends Controller
 
             this.manager.getUsers()
             .then(response => this._fetchSituations(response))
-            .then(response => this.view.updateTable(response));
-
-            document.querySelector('h2').addEventListener('click', this._orderByName());
+            .then(response => this.view.updateTable(response))
+            .then(response => document.querySelector('#thName').addEventListener('click', this._orderByName()));
     }
 
     _orderByName(event) {
-        const lines = document.querySelectorAll('#tableUsers tbody tr');
+        const tableBody = document.querySelector('tbody');
+        const nodeList = document.querySelectorAll('tbody tr');
+        const lines = Array.from(nodeList);
+        
+//        lines.sort(this._compareNameAsc);
+
+        lines.sort(this._compareNameDesc);
+
+        tableBody.innerHTML = '';
+       
+        for (let i = 0; i < lines.length; i++) {
+            tableBody.appendChild(lines[i]);
+        }
+        
         console.log(lines);
+    }
+
+    _compareNameAsc(line1, line2) {
+        const name1 = line1.cells[1].innerHTML;
+        const name2 = line2.cells[1].innerHTML;
+
+        return name1.localeCompare(name2);
+    } 
+    _compareNameDesc(line1, line2) {
+        const name1 = line1.cells[1].innerHTML;
+        const name2 = line2.cells[1].innerHTML;
+
+        return name2.localeCompare(name1);
     }
 }
